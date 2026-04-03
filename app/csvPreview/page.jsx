cfecbaded7f83f1de9bbf323938d5641690bf750
MrTalone <talon.finehout@western.edu>
2026-04-03 15:52:02 -0600

    fix non used files

--- app/csvPreview/page.jsx
+++ app/csvPreview/page.jsx
@@ -1,23 +1,23 @@
-//Acts as the daily import for now
+// //Acts as the daily import for now
 
-import { buildCsvStoreData } from '../components/fetchData'
-import { supabase } from '@/lib/supabase'
-import { redirect } from 'next/navigation'
-import { importService} from '@/lib/database/ImportDataService.jsx'
-// Define which columns to display
-const COLUMNS_TO_SHOW = ['StationName','BI','ERC','ObservationTime','NFDRType', 'Fuel_Model']
+// import { buildCsvStoreData } from '../components/fetchData'
+// import { supabase } from '@/lib/supabase'
+// import { redirect } from 'next/navigation'
+// import { importService} from '@/lib/database/ImportDataService.jsx'
+// // Define which columns to display
+// const COLUMNS_TO_SHOW = ['StationName','BI','ERC','ObservationTime','NFDRType', 'Fuel_Model']
 
-// Server component that displays CSV data directly
-export default async function CsvPreviewPage({ searchParams }) {
-  return
-  // Fetch CSV data from the server
-  //const { rows, columnNames, summary } = await importService.runDailyImport() //fetch and import data, return rows for preview and summary of import results
-  // Fetch dispatch areas for the select menu
-  // const { data: FDRA } = await supabase
-  //   .from('FDRA')
-  //   .select('FDRA_ID, FDRAname')
-  //   .order('FDRAname')
+// // Server component that displays CSV data directly
+// export default async function CsvPreviewPage({ searchParams }) {
+//   return
+//   // Fetch CSV data from the server
+//   //const { rows, columnNames, summary } = await importService.runDailyImport() //fetch and import data, return rows for preview and summary of import results
+//   // Fetch dispatch areas for the select menu
+//   // const { data: FDRA } = await supabase
+//   //   .from('FDRA')
+//   //   .select('FDRA_ID, FDRAname')
+//   //   .order('FDRAname')
 
-  // Display first 10 rows
-  //const displayRows = rows.slice(0, 10)
-}  
+//   // Display first 10 rows
+//   //const displayRows = rows.slice(0, 10)
+// }  
